526b4ef27c3f675e861f45cd807769da9786c153
projecttemp <jasonyu932@gmail.com>
2015-03-17 11:04:06 -0400

    Major update on the backend, most functions been added

--- Java/BackEnd.java
+++ Java/BackEnd.java
@@ -4,7 +4,9 @@
 //		   the Users and Events file with any change from the transaction. The 
 //		   transaction file will be blanked afterward for next day.
 
+import java.lang.NullPointerException;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -17,25 +19,135 @@ public class BackEnd{
 	static final String TransactionFile = "TransactionFile.txt";
 
 	public static void main(String[] Argv){
-		BufferedReader Users = readFile(UsersFile);
-		BufferedReader Events = readFile(EventsFile);
-		BufferedReader Transaction = readFile(TransactionFile);
+		BufferedReader Users = readFromFile(UsersFile);
+		BufferedReader Events = readFromFile(EventsFile);
+		BufferedReader Transaction = readFromFile(TransactionFile);
+		HashMap<String, User> userData = new HashMap<String, User>();
+		HashMap<String, Event> eventData = new HashMap<String, Event>();
+		String line, name;
+		User u;
+		Event e;
+		int amount;
+		int transType;
+		float value;
+		
+		try{
+			//Gets all the user data from the file and put it in the hash map one line at a time
+			line = Users.readLine();
+			while(line != null){
+				name = line.substring(0, 15).trim();
+				value = Float.parseFloat(line.substring(19,28));
+				u = new User(name, line.substring(16, 18), value);
+				
+				userData.put(name, u);
+				line = Users.readLine();
+			}
+			
+			//Gets all the event data from the file and put it in a hash map
+			line = Events.readLine();
+			while(line != null){
+				name = line.substring(0, 19).trim();
+				amount = Integer.parseInt(line.substring(34, 37));
+				value = Float.parseFloat(line.substring(38,44));
+				e = new Event(name, line.substring(20, 33).trim(), amount, value);
+				
+				eventData.put(name, e);
+				line = Events.readLine();
+			}
+			
+			//Gets each transaction one at a time and apply the change to the user or event database
+			line = Transaction.readLine();
+			while(line != null){
+				transType = Integer.parseInt(line.substring(0, 3));
+				
+				if(transType == 1){ //Creating an account
+					name = line.substring(3, 18).trim();
+					
+					if(userData.get(name) != null){
+						System.out.println("ERROR: The user " + name + " already exist in the database.");
+					} else {
+						value = Float.parseFloat(line.substring(22, 31));
+						u = new User(name, line.substring(19,21), value);						
+						userData.put(name, u);
+					}
+				} else if(transType == 2){	//Deleting an account
+					name = line.substring(3, 18).trim();
+					u = userData.remove(name);
+					
+					if(u == null){
+						System.out.println("ERROR: The user " + name + " cannot be removed because it no longer exist.");
+					}
+				} else if(transType == 3){	//Selling tickets for new event
+				
+				} else if(transType == 4){	//Buying from an event
+				
+				} else if(transType == 5){	//Refund from seller to buyer
+					value = Float.parseFloat(line.substring(35, 44));
+					
+					name = line.substring(3, 18).trim();
+					u = userData.remove(name);
+					
+					if(u == null){
+						System.out.println("ERROR: The user " + name + " cannot be refunded because it no longer exist.");
+						//No point in subtracting the fund from seller if the buyer no longer exist
+					} else {
+						u.setCredit(u.getCredit() + value);
+						userData.put(name, u);
+						
+						name = line.substring(19, 34).trim();
+						u = userData.remove(name);
+						
+						if(u == null){
+							System.out.println("ERROR: The user " + name + " cannot be initiate the refund because it no longer exist.");
+							
+							//If the seller is gone, we can't just give buyer free money
+							name = line.substring(3, 18).trim();
+							u = userData.remove(name);
+							u.setCredit(u.getCredit() - value);
+							userData.put(name, u);
+						} else {
+							u.setCredit(u.getCredit() - value);
+							userData.put(name, u);
+						}
+					}
+				} else if(transType == 6){	//Add credit to an account
+					name = line.substring(3, 18).trim();
+					value = Float.parseFloat(line.substring(22, 31));
+					u = userData.remove(name);
+					
+					if(u == null){
+						System.out.println("ERROR: Credit cannot be added to the user " + name + " because it no longer exist.");
+					} else {
+						u.setCredit(u.getCredit() + value);
+						userData.put(name, u);
+					}
+				}
+				
+				line = Transaction.readLine();
+			}
+		} catch (IOException ex){
+			System.out.println("ERROR: IO Exception???? Should never occur!");
+			System.exit(1);
+		} catch (NullPointerException exp){
+			System.out.println("ERROR: Null pointer?????Should never occur!");
+			System.exit(1);
+		}
 	}
 	
 	//Read a file name and provided a buffered reader for the file
 	//Also handle error relating to file not being found
-	public static BufferedReader readFile(String fileName){
+	public static BufferedReader readFromFile(String name){
 		try{
-			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			BufferedReader br = new BufferedReader(new FileReader(name));
 			return br;
 		} catch (FileNotFoundException e){
-			System.out.println("ERROR: File " + fileName + " not found!");
+			System.out.println("ERROR: File " + name + " not found!");
 			return null;
 		}
 	}
 	
 	//Write all the data inside the user and event map to the given file
-	public static int writeFile(String fileName, HashMap<String, User> userData, HashMap<String, Event> eventData){
+	public static int writeToFile(String name, HashMap<String, User> u, HashMap<String, Event> e){
 		return 1;
 	}
 }
